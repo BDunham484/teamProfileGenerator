f941a66ded0d6ca1ff85acd17f66e69344c9fbff
ThereWolf <bdunham484@gmail.com>
2022-07-09 17:24:49 -0500

    add intern methods and tests

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -14,33 +14,33 @@ test('creates an intern object', () => {
 test("gets intern's name", () => {
     const intern = new Intern('Rick');
 
-    expect(intern.name).toBe('Rick');
+    expect(intern.getName()).toEqual(expect.any(String));
 });
 
 
 test("gets intern's id", () => {
     const intern = new Intern('Rick');
 
-    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.getId()).toEqual(expect.stringContaining(intern.id.toString()));
 });
 
 
 test("gets intern's email", () => {
     const intern = new Intern('Rick');
 
-    expect(intern.email).toEqual(expect.stringContaining('@'));
+    expect(intern.getEmail()).toEqual(expect.stringContaining('@'));
 });
 
 
 test("gets intern's school", () => {
     const intern = new Intern('Rick');
 
-    expect(intern.school).toEqual(expect.any(String));
+    expect(intern.getSchool()).toEqual(expect.any(String));
 })
 
 
 test("gets intern's role", () => {
     const intern = new Intern('Rick');
 
-    expect(intern.role).toEqual(expect.any(String));
+    expect(intern.getRole()).toEqual(expect.any(String));
 });
